8cfcb2110bdeda0e5289235271ce4afb5302e0ad
FMcIntosh <frasermcintosh1@gmail.com>
2017-05-19 01:48:43 -0700

    Styling display

--- components/DisplaySection/index.js
+++ components/DisplaySection/index.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 import SectionTitle from './SectionTitle';
+import SectionIcon from './SectionIcon';
 import DisplayItem from './DisplayItem';
 
 export default (props) => (
     <Wrapper dark={props.dark}>
      <SectionTitle>Developing</SectionTitle>
+     <SectionIcon src="https://d30y9cdsu7xlg0.cloudfront.net/png/62495-200.png"/>
      <DisplayItem dark={props.dark}/>
      <DisplayItem dark={props.dark}/>
      <DisplayItem dark={props.dark}/>
@@ -14,5 +16,8 @@ export default (props) => (
 
 const Wrapper = styled.div`
     padding-top: 15vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     background: ${props => props.dark ? '#222326': 'white'};
 `
