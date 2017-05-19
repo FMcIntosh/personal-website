190cb745cc7f778240af28c4bd242f8aa1e9e8ec
FMcIntosh <frasermcintosh1@gmail.com>
2017-05-19 14:55:13 -0700

    Add border and reduce width

--- components/SplashScreen/Title.js
+++ components/SplashScreen/Title.js
@@ -2,8 +2,11 @@ import styled from 'styled-components';
 import { orange } from '~/styles/colors';
 
 export default styled.h1`
+  width: 50%;
   font-size: 100px;
   text-align: center;
   color: ${orange};
   font-family: 'Roboto', sans-serif;
+  border-bottom: 2px solid white;
+  padding-bottom: 30px;
 `;
